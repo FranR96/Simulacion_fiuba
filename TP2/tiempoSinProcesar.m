143cfa3ec985cda9e32803d9e24d664b673fe04e
FranR96 <32714902+FranR96@users.noreply.github.com>
2018-10-29 15:21:00 -0300

    Update tiempoSinProcesar.m

--- TP2/tiempoSinProcesar.m
+++ TP2/tiempoSinProcesar.m
@@ -1,9 +1,9 @@
 function promedio = tiempoSinProcesar(z)
   cant0 = 0;
-  for i =1:10000
+  for i =1:100000
     if z(i)==0
       cant0 =cant0 +1;
     endif
   endfor
-  promedio = cant0/10000;
+  promedio = cant0/100000;
 endfunction
